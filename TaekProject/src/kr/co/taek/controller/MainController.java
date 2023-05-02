9e98402ef33cf2f16ccaaf96802cd082300b2233
yongtaek <laundryday@naver.com>
2023-05-02 23:04:11 +0900

    게시판 mybatis 연동 전

---
+++ TaekProject/src/kr/co/taek/controller/MainController.java
@@ -0,0 +1,12 @@
+package kr.co.taek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping("/main")
+	public String main() {
+		return "main";
+	}
+}
