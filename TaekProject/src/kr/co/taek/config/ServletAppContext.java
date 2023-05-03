e4f13996b16c8698da6c6ff03a010bb102ae9888
yongtaek <laundryday@naver.com>
2023-05-03 21:22:45 +0900

    mabatis 설정

--- TaekProject/src/kr/co/taek/config/ServletAppContext.java
+++ TaekProject/src/kr/co/taek/config/ServletAppContext.java
@@ -1,19 +1,43 @@
 package kr.co.taek.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import kr.co.taek.mapper.BoardMapper;
 
 // Spring MVC 프로젝트에 관련된 설정을 하는 클래스
 @Configuration
 // Controller 어노테이션이 셋팅되어 있는 클래스를 Controller로 등록한다.
 @EnableWebMvc
 // 스캔할 패키지를 지정한다.
-@ComponentScan("kr.co.taek.controller")
+@ComponentScan("kr.co.softcampus.controller")
+
+@PropertySource("/WEB-INF/properties/db.properties")
 public class ServletAppContext implements WebMvcConfigurer{
+	
+	@Value("${db.classname}")
+	private String db_classname;
+	
+	@Value("${db.url}")
+	private String db_url;
+	
+	@Value("${db.username}")
+	private String db_username;
+	
+	@Value("${db.password}")
+	private String db_password;
+	
 	// Controller의 메서드가 반환하는 jsp의 이름 앞뒤에 경로와 확장자를 붙혀주도록 설정한다.
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
@@ -29,8 +53,36 @@ public class ServletAppContext implements WebMvcConfigurer{
 		WebMvcConfigurer.super.addResourceHandlers(registry);
 		registry.addResourceHandler("/**").addResourceLocations("/resources/");
 	}
+	
+	// 데이터베이스 접속 정보를 관리하는 Bean
+	@Bean
+	public BasicDataSource dataSource() {
+		BasicDataSource source = new BasicDataSource();
+		source.setDriverClassName(db_classname);
+		source.setUrl(db_url);
+		source.setUsername(db_username);
+		source.setPassword(db_password);
+		
+		return source;
+	}
+	
+	// 쿼리문과 접속 정보를 관리하는 객체
+	@Bean
+	public SqlSessionFactory factory(BasicDataSource source) throws Exception{
+		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+		factoryBean.setDataSource(source);
+		SqlSessionFactory factory = factoryBean.getObject();
+		return factory;
+	}
+	
+	// 쿼리문 실행을 위한 객체(Mapper 관리)
+	@Bean
+	public MapperFactoryBean<BoardMapper> getBoardMapper(SqlSessionFactory factory) throws Exception{
+		MapperFactoryBean<BoardMapper> factoryBean = new MapperFactoryBean<BoardMapper>(BoardMapper.class);
+		factoryBean.setSqlSessionFactory(factory);
+		return factoryBean;
+	}
 }
-
 
 
 
